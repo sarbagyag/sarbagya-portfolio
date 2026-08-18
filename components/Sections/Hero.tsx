4a147a024ecb124f5c7f0c21b0c650319c3265d4
sarbagya <sarbagyaghoshrestha@gmail.com>
2026-08-18 21:55:02 +0545

    fix: fix the size of motto text

--- components/Sections/Hero.tsx
+++ components/Sections/Hero.tsx
@@ -71,7 +71,7 @@ const Hero: React.FC<{ profile: Profile }> = ({ profile }) => {
           )}
 
           {profile.heroMotto && (
-            <p className="italic text-base sm:text-lg text-text-secondary max-w-md leading-relaxed mb-10">
+            <p className="italic text-sm sm:text-base text-text-tertiary max-w-md leading-relaxed mb-10">
               {profile.heroMotto}
             </p>
           )}
